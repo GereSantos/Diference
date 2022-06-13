bb9cd135d6bc6c74e92c66127d05903fd95e8103
Geremias Santos <Geremias99999@gmail.com>
2022-06-13 09:22:23 -0300

    Exercise Diference

---
+++ Exercise (Diference)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise__Diference_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que calcula e mostra a diferença entre Produto A * B - C * D
+
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+            int dif = 0;
+
+            dif = (A * B - C * D);
+
+            Console.WriteLine("Diferença " + dif );
+
+        }
+    }
+}
